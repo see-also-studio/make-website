f240d48e1ab83eeb77e2aa522be4c834fcbb765e
m <mike@ms0.co.uk>
2021-07-19 14:44:09 +0100

    Add Terser js minify plugin

--- .eleventy.js
+++ .eleventy.js
@@ -28,6 +28,19 @@ module.exports = function(eleventyConfig) {
     return content;
   });
 
+  // Terser, minify javascript
+  const { minify } = require('terser');
+  eleventyConfig.addNunjucksAsyncFilter('jsmin', async function(code, callback) {
+    try {
+      const minified = await minify(code);
+      callback(null, minified.code);
+    } catch (err) {
+      console.error('Terser error: ', err);
+      // Fail gracefully.
+      callback(null, code);
+    }
+  });
+
 
   // Markdown-it, add target _blank to external markdown links
   const md = new markdownIt({
