c098a2271ebf480826d696bf481aabaa1c1a6e82
m <mike@ms0.co.uk>
2021-07-26 16:16:55 +0100

    Init basic srcset for image gallery

--- src/_data/helper.js
+++ src/_data/helper.js
@@ -38,5 +38,43 @@ module.exports = {
       string = 'full';
     }
     return string;
+  },
+
+  srcset(src, type) {
+    let string = '';
+
+    switch (type) {
+      case 'single':
+      case 'double':
+      case 'full':
+        string += src + '?nf_resize=fit&w=' + 320 + '&h=' + Math.ceil(320 * 0.64) + ' ' + 320 + 'w,';
+        string += src + '?nf_resize=fit&w=' + 640 + '&h=' + Math.ceil(640 * 0.64) + ' ' + 640 + 'w,';
+        string += src + '?nf_resize=fit&w=' + 768 + '&h=' + Math.ceil(768 * 0.64) + ' ' + 768 + 'w,';
+        string += src + '?nf_resize=fit&w=' + 1024 + '&h=' + Math.ceil(1024 * 0.64) + ' ' + 1024 + 'w,';
+        string += src + '?nf_resize=fit&w=' + 1366 + '&h=' + Math.ceil(1366 * 0.64) + ' ' + 1366 + 'w,';
+        string += src + '?nf_resize=fit&w=' + 1600 + '&h=' + Math.ceil(1600 * 0.64) + ' ' + 1600 + 'w,';
+        string += src + '?nf_resize=fit&w=' + 1920 + '&h=' + Math.ceil(1920 * 0.64) + ' ' + 1920 + 'w';
+        break;
+    }
+
+    return string;
+  },
+
+  sizes(type) {
+    let string = '';
+
+    switch (type) {
+      case 'single':
+        string = '(max-width: 679px) 100vw, 75vw';
+        break;
+      case 'double':
+        string = '50vw';
+        break;
+      case 'full':
+        string = '(max-width: 679px) 100vw, 65vw';
+        break;
+    }
+
+    return string;
   }
 };
