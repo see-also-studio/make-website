73f72e80e0397046ef53dce5941dc820a48f00ce
m <mike@ms0.co.uk>
2021-07-28 18:41:42 +0100

    Update activityCards.js
    Make filters wrapper resize height dependent on number of filters shown.

--- src/build/scripts/components/activityCards.js
+++ src/build/scripts/components/activityCards.js
@@ -13,11 +13,13 @@ const filterEls = document.querySelectorAll('.activities__filters .activities__f
 let filtersActive = [];
 
 filterEls.forEach(function(el) {
-  el.addEventListener('click', function() {
-    const filter = el.getAttribute('data-filter');
-    el.classList.toggle('activities__filter--active');
+  el.addEventListener('click', function(e) {
+    const filterEl = e.currentTarget;
+    const filter = filterEl.getAttribute('data-filter');
+    filterEl.classList.toggle('activities__filter--active');
+    setFiltersHeight(filterEl.parentNode);
 
-    if (el.classList.contains('activities__filter--active')) {
+    if (filterEl.classList.contains('activities__filter--active')) {
       addFilter(filter);
     } else {
       removeFilter(filter);
@@ -62,5 +64,21 @@ document.querySelectorAll('.activities__filter--toggle').forEach(function(el) {
     const parent = el.parentNode;
     parent.classList.toggle('activities__filter--hidden');
     el.innerHTML = parent.classList.contains('activities__filter--hidden') ? 'Show filters' : 'Hide filters';
+    setFiltersHeight(parent);
   });
 });
+
+function setFiltersHeight(wrapper) {
+  const hidden = wrapper.classList.contains('activities__filter--hidden') ? true : false;
+
+  if (hidden) {
+    const filterHeight = wrapper.querySelector('.activities__filter--toggle').offsetHeight;
+    const activeFilters = wrapper.querySelectorAll('.activities__filter--active');
+    let lastFilter = activeFilters.length ? activeFilters[activeFilters.length -1] : null;
+    wrapper.style.height = lastFilter ? lastFilter.offsetTop + filterHeight + 'px' : filterHeight + 'px';
+  } else {
+    //const last = wrapper.lastElementChild;
+    //wrapper.style.height = last.offsetHeight + last.offsetTop + 'px';
+    wrapper.style.height = 'auto';
+  }
+}
