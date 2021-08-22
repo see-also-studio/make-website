f6a51e0426dfcc2047f701a7f636dd083a11b5c4
m <mike@ms0.co.uk>
2021-08-22 19:31:09 +0100

    Simplify transparent header

--- src/build/scripts/components/index.js
+++ src/build/scripts/components/index.js
@@ -1,8 +1,6 @@
 const header = document.querySelector('.header');
 const indexHeader = document.querySelector('.index-header');
 const mainContent = document.querySelector('#main');
-const headerLogo = document.querySelector('.header__logo');
-const headerWrapper = document.querySelector('.header__top');
 
 if (indexHeader) {
   scrollHandler();
@@ -11,10 +9,7 @@ if (indexHeader) {
 
 function scrollHandler() {
   const headerVisible = mainContent.getBoundingClientRect().top >= 5 ? true : false;
-
-  indexHeader.classList.toggle('index-header--hidden', !headerVisible);
-  headerLogo.classList.toggle('header__logo--active', !headerVisible);
-  headerWrapper.classList.toggle('header__top--transparent', headerVisible);
+  header.classList.toggle('header--transparent', headerVisible);
 }
 
 window.addEventListener('scroll', stickyMenu);
